e6e1eecf2c357e75e33bdaa0ee98a8ee23094c5a
vinayak-meltwater <vinayak.gaonkar@meltwater.com>
2021-09-06 16:15:19 +0530

    fix: working hour constraints addedd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const { App } = require('@slack/bolt');
+const moment=require('moment-timezone');
 
 const app = new App({
     token: process.env.BOT_USER_TOKEN,
@@ -9,16 +10,24 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN
   });
 
-const currentTime = new Date().toTimeString();
+
 
 // Listens to incoming messages that contain "hello"
 app.message(async ({ message, say }) => {
-    console.log("got message");
+  const currentTime = new Date();
+  var currentHour = moment(currentTime).tz('Asia/Kolkata').format('HH')
+  console.log(currentHour);
+  // console.log(today.tz('Asia/Kolkata').format('HH'));
+  
   var threadTs;
   if(message.thread_ts){threadTs = message.thread_ts; }else{threadTs=message.ts;}
   // say() sends a message to the channel where the event was triggered
   
-  await say({text:`Hey there <@${message.user}>!`,thread_ts:threadTs});
+  if(currentHour<10 || currentHour >19 ){
+    await say({text:`Unfortunately <@${message.user}>! your ping has come while <@spr> is offline currently. team will replay during the normal (IST) working hour
+    For service-affecting issues, please page our on-call engineer for immediate response through slack /genie alert __alert_message__ for sprinters.
+    `,thread_ts:threadTs});
+  }
 });
 
 (async () => {
